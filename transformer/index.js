f6eba34d149c90de71a0dd773941d5098d8554dc
Boy with Silver Wings <agney@outlook.in>
2019-06-17 22:47:37 +0530

    pass values to output

--- transformer/index.js
+++ transformer/index.js
@@ -1,10 +1,14 @@
 const fare = require("./fare");
 const stations = require("./stations");
+const output = require("./output");
 
 async function init() {
   const fareDetails = await fare();
   const stationDetails = await stations();
-  console.log(stationDetails);
+  output(
+    { name: "fareDetail", value: fareDetails },
+    { name: "stations", value: stationDetails }
+  );
 }
 
 init();
---
+++ transformer/output.js
@@ -0,0 +1,9 @@
+/**
+ * Create output files in the app directory for frontend to consume.
+ * @param {Object[]} values - values to write to files
+ * @param {string} values.name - of file
+ * @param {any} values.value - to be written
+ */
+function output(...values) {}
+
+module.exports = output;
