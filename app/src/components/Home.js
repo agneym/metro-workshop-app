6b8a0fb961d7a559301f4180010005ea77091f43
Boy with Silver Wings <agney@outlook.in>
2019-06-18 23:51:26 +0530

    setup contact us section

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -29,6 +29,42 @@ function Home() {
           </ul>
         </div>
       </section>
+      <form className="section">
+        <div className="container">
+          <h2 className="title">Contact Us</h2>
+          <div className="field">
+            <label className="label">Name</label>
+            <div className="control">
+              <input className="input" type="text" placeholder="Name" />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input className="input" type="email" placeholder="Email" />
+            </div>
+          </div>
+          <div className="field">
+            <label className="label">Message</label>
+            <div className="control">
+              <textarea
+                className="textarea"
+                placeholder="Add message here"
+              ></textarea>
+            </div>
+          </div>
+          <div className="field">
+            <div className="control has-text-centered">
+              <button className="button is-primary">Message</button>
+            </div>
+          </div>
+        </div>
+      </form>
+      <footer className="footer has-background-dark has-text-white">
+        <div className="content has-text-centered">
+          <p>Website by Boy with Silver Wings</p>
+        </div>
+      </footer>
     </main>
   );
 }
