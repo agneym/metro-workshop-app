f8c7ea106a7cadda62deb2ca0b4c867e14a4bb63
Boy with Silver Wings <agney@outlook.in>
2019-06-18 23:00:14 +0530

    add a header on home section

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Home() {
-  return <p>Home</p>;
+  return (
+    <section className="hero is-medium is-primary is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Kochi Metro</h1>
+          <h2 className="subtitle">Most Advanced Metro System in India</h2>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
