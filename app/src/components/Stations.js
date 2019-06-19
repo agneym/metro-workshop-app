d7e157649aa233736acb9be9239999dad8b83a60
Boy with Silver Wings <agney@outlook.in>
2019-06-19 22:47:56 +0530

    list stations with map

--- app/src/components/Stations.js
+++ app/src/components/Stations.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+import stationData from "../data/stations";
 import Header from "./Header";
 
 function Stations() {
@@ -7,19 +9,23 @@ function Stations() {
       <Header subtitle="Station Information" />
       <section className="section">
         <div className="container">
-          <article className="media">
-            <figure className="media-left">
-              <p className="image is-64x64">
-                <img
-                  src="https://bulma.io/images/placeholders/128x128.png"
-                  alt=""
-                />
-              </p>
-            </figure>
-            <div class="media-content">
-              <div class="content"></div>
-            </div>
-          </article>
+          {stationData.map(station => (
+            <article className="media" key={station.stop_id}>
+              <figure className="media-left">
+                <p className="image is-64x64">
+                  <img
+                    src={"https://bulma.io/images/placeholders/128x128.png"}
+                    alt=""
+                  />
+                </p>
+              </figure>
+              <div className="media-content">
+                <div className="content">
+                  <strong>{station.stop_name}</strong>
+                </div>
+              </div>
+            </article>
+          ))}
         </div>
       </section>
     </main>
