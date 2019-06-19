25aff4617ec3c8ce4b2badb781f041ade58791df
Boy with Silver Wings <agney@outlook.in>
2019-06-19 23:16:39 +0530

    calculate fare from stations

--- app/src/components/Fare.js
+++ app/src/components/Fare.js
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
+import stationData from "../data/stations";
+import fareData from "../data/fareDetail";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function Fare() {
+  const [starting, setStarting] = useState("ALVA");
+  const [destination, setDestination] = useState("MACE");
+  const [fare, setFare] = useState("");
+  const calculateFare = event => {
+    event.preventDefault();
+    const result = fareData.find(
+      station => station.originId === starting && station.destId === destination
+    );
+    const resultingFare = result.fare;
+    setFare(resultingFare);
+  };
   return (
     <main>
       <Header subtitle="Fare Calculator" />
+      <section className="section">
+        <form className="container level" onSubmit={calculateFare}>
+          <div className="select is-rounded">
+            <label htmlFor="">Starting</label>
+            <select
+              value={starting}
+              onChange={event => setStarting(event.target.value)}
+            >
+              {stationData.map(station => (
+                <option value={station.stop_id}>{station.stop_name}</option>
+              ))}
+            </select>
+          </div>
+          <div className="select is-rounded">
+            <label htmlFor="">Destination</label>
+            <select
+              value={destination}
+              onChange={event => setDestination(event.target.value)}
+            >
+              {stationData.map(station => (
+                <option value={station.stop_id}>{station.stop_name}</option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <button className="button is-primary is-rounded">Calculate</button>
+          </div>
+        </form>
+        {fare && (
+          <div className="container section">
+            <h2 className="subtitle">Your fare is</h2>
+            <h1 className="title has-text-success">₹ {fare}</h1>
+          </div>
+        )}
+      </section>
+      <Footer />
     </main>
   );
 }
