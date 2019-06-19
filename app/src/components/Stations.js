3543cdeed3fe68333f2025a8ac5b28438574d4f2
Boy with Silver Wings <agney@outlook.in>
2019-06-19 22:21:17 +0530

    add a sample media content

--- app/src/components/Stations.js
+++ app/src/components/Stations.js
@@ -5,6 +5,23 @@ function Stations() {
   return (
     <main>
       <Header subtitle="Station Information" />
+      <section className="section">
+        <div className="container">
+          <article className="media">
+            <figure className="media-left">
+              <p className="image is-64x64">
+                <img
+                  src="https://bulma.io/images/placeholders/128x128.png"
+                  alt=""
+                />
+              </p>
+            </figure>
+            <div class="media-content">
+              <div class="content"></div>
+            </div>
+          </article>
+        </div>
+      </section>
     </main>
   );
 }
