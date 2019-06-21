b353f977603d4799d7ef28b81261aecfd1a6744f
Boy with Silver Wings <agney@outlook.in>
2019-06-21 22:52:33 +0530

    Updates

---
+++ precache-manifest.cd2642bcca78fb9402eaa5fe81d25146.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a43822f129a7b2bdd607b3cf5bf56fad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67b1f9790e4a9989bc9e",
+    "url": "/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "5944be96348cef24b2b6",
+    "url": "/static/css/main.c4fe3570.chunk.css"
+  },
+  {
+    "revision": "67b1f9790e4a9989bc9e",
+    "url": "/static/js/2.620ef451.chunk.js"
+  },
+  {
+    "revision": "5944be96348cef24b2b6",
+    "url": "/static/js/main.6d26ae3e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
